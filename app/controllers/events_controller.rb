e06a6ff6732f449a18c4cc47d7aefe088c1c2197
zbosnjak <zbosnjak@gmail.com>
2022-07-19 17:55:17 +0200

    New again

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,34 +4,47 @@ class EventsController < ApplicationController
         @event = Event.new
     end
 
+    
     def create
         @event = Event.new(event_params)
-    
         if @event.save
-          flash[:notice] = "Event created!"
-          redirect_to @event
+            flash[:notice] = "Event created!"
+            redirect_to @event
         else
-          flash.now[:alert] = "Event not created"
-          render "new"
+            flash.now[:alert] = "Event not created"
+            render "new"
         end
-      end
-
-      def show
-        @event = Event.find(params[:id])
-      end
-
-      def index
-        @events = Event.all
-      end
-
-
-      private
-
-      def event_params
-        params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location, :image, :category_id, :seats, :tag_list)
-      end
-
     end
+
+private
+
+    def event_params
+        params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
+    end
+
+end
+
+
+#    
+        
+          
+          
+
+          
+#      end
+
+#      def show
+#        @event = Event.find(params[:id])
+#      end
+
+#      def index
+#        @events = Event.all
+#      end
+
+
+    
+
+#    end
 
 
 
@@ -99,7 +112,7 @@ class EventsController < ApplicationController
       #     flash[:alert] = "You do not have enough permission to '#{action_name}' the '#{@event.title.upcase}' event"
       #     redirect_to events_path
       #   end
-      # end
+
   
   
   
